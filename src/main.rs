cd789de0859ee2f27e5f408fa235f4ebf7f3fdaf
TeaSpillStudios <teaspillstudios@proton.me>
2023-03-12 21:49:13 +0000

    Separate the handler into a new function

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,17 @@
-use std::fs;
-
 use axum::{response::Html, routing::get, Router};
-use markdown::to_html;
+use std::{fs, path::Path};
+
+async fn handler() -> Html<String> {
+    let html = fs::read_to_string("./resources/index.html").expect("Failed to load `index.html`");
+    let markdown = markdown::file_to_html(Path::new("./resources/index.md")).unwrap();
+
+    Html(html + &markdown + "</body>")
+}
 
 #[tokio::main]
 async fn main() {
     // Build a new app
-    let app = Router::new().route(
-        "/",
-        get(|| async {
-            let markdown = fs::read_to_string("./resources/index.md").unwrap();
-            let css = fs::read_to_string("./resources/styles.css").unwrap();
-            let html = to_html(&markdown);
-
-            let css_and_html = format!(
-                "<!DOCTYPE html>\n\n<head>\n<style>\n{}</style>\n</head>\n\n<body>\n{}</body>",
-                css, html
-            );
-
-            Html(css_and_html)
-        }),
-    );
+    let app = Router::new().route("/", get(handler));
 
     // Bind it on localhost:3000
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
