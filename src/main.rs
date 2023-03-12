019f4ca42c0ad3a5c9b34a20d20254f5cb0708d7
TeaSpillStudios <teaspillstudios@proton.me>
2023-03-12 16:56:58 +0000

    Server Markdown as HTML

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+
+use axum::{response::Html, routing::get, Router};
+use markdown::to_html;
+
+#[tokio::main]
+async fn main() {
+    // build our application with a single route
+    let app = Router::new().route(
+        "/",
+        get(|| async {
+            let markdown = fs::read_to_string("./index.md");
+            let html = to_html(&markdown.unwrap());
+
+            Html(html)
+        }),
+    );
+
+    // run it with hyper on localhost:3000
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
