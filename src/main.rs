8c89243fe1e86bb1da452d40a3b785f4f9457a22
TeaSpillStudios <teaspillstudios@proton.me>
2023-03-12 23:24:47 +0000

    Load from static

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,19 @@
-use axum::{response::Html, routing::get, Router};
-use std::{fs, path::Path};
-
-async fn handler() -> Html<String> {
-    let html = fs::read_to_string("./resources/index.html").expect("Failed to load `index.html`");
-    let markdown = markdown::file_to_html(Path::new("./resources/index.md")).unwrap();
-
-    Html(html + &markdown + "</body>")
-}
+use axum::{response::Html, routing::get, Router, Server};
+use std::fs;
+use tower_http::{services::ServeDir, trace::TraceLayer};
 
 #[tokio::main]
 async fn main() {
-    // Build a new app
-    let app = Router::new().route("/", get(handler));
+    let app = Router::new()
+        .nest_service("/static", ServeDir::new("static"))
+        .route("/", get(handler));
 
-    // Bind it on localhost:3000
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
-        .serve(app.into_make_service())
+    Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.layer(TraceLayer::new_for_http()).into_make_service())
         .await
         .unwrap();
+}
+
+async fn handler() -> Html<String> {
+    Html(fs::read_to_string("static/index.html").expect("Failed to load HTML"))
 }
