56e0db1d09818e4b2c9d5fbe9ea2784d16662336
TeaSpillStudios <teaspillstudios@proton.me>
2023-03-21 00:06:19 +0000

    Add a dev mode

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use markdown::file_to_html;
 use std::{fs, path::Path};
 use tower_http::{services::ServeDir, trace::TraceLayer};
 
+const DEV_MODE: bool = true;
+
 #[tokio::main]
 async fn main() {
     let app = Router::new()
@@ -16,14 +18,30 @@ async fn main() {
 }
 
 async fn handler() -> Html<String> {
-    let html = fs::read_to_string("static/index.html").expect("Failed to load HTML");
-    let footer_html = fs::read_to_string("static/footer.html")
+    let html_path = if DEV_MODE {
+        "static/index.html"
+    } else {
+        "/web-resources/index.html"
+    };
+    let footer_html_path = if DEV_MODE {
+        "static/index.html"
+    } else {
+        "/web-resources/index.html"
+    };
+    let markdown_path = if DEV_MODE {
+        "static/index.html"
+    } else {
+        "/web-resources/index.html"
+    };
+
+    let html = fs::read_to_string(html_path).expect("Failed to load HTML");
+    let footer_html = fs::read_to_string(footer_html_path)
         .expect("Failed to load footer HTML")
         .lines()
         .map(|x| format!("    {x}\n"))
         .collect::<String>();
 
-    let markdown = file_to_html(Path::new("static/index.md"))
+    let markdown = file_to_html(Path::new(markdown_path))
         .expect("Failed to convert Markdown to HTML")
         .lines()
         .map(|x| format!("    {x}\n"))
